e60627aa8fd95779d4b9e6e7ac8104b3e59e1979
Xianghu Zhao <xianghuzhao@gmail.com>
2019-02-22 23:14:42 +0800

    Cli tag list

--- lib/procli/cli.rb
+++ lib/procli/cli.rb
@@ -1,10 +1,21 @@
 require "thor"
- 
+
+require "procli/cli/tag"
+
 module Procli
-	class Cli < Thor
-    desc "hello NAME", "say hello to NAME"
-    def hello(name)
-      puts "Hello #{name}"
+  module Cli
+    class Main < Thor
+      class_option :endpoint, type: :string, aliases: :e,
+        default: ENV['PROCRAM_API_URL'] || 'http://localhost:8080/'
+
+      desc "hello NAME", "say hello to NAME"
+      def hello(name)
+        puts options[:endpoint]
+        puts "Hello #{name}"
+      end
+
+      desc "tag SUBCOMMAND ...ARGS", "manage tags"
+      subcommand "tag", Tag
     end
-	end
+  end
 end
---
+++ lib/procli/cli/tag.rb
@@ -0,0 +1,22 @@
+require "thor"
+require "http"
+
+module Procli
+  module Cli
+    class Tag < Thor
+      option :limit, type: :numeric, aliases: :l
+      desc "list [--limit N]", "List tags"
+      def list()
+        puts parent_options[:endpoint]
+        puts HTTP.get(parent_options[:endpoint]).body.to_s
+      rescue => e
+        puts e
+      end
+
+      desc "create TAG_NAME", "Create tag with the provided name"
+      def create(name)
+        puts "Create tag: #{name}"
+      end
+    end
+  end
+end
